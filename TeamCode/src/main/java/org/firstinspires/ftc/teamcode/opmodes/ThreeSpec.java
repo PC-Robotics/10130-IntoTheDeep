7737bf864b145005077713f32bf2d493c67472b5
Dennis Colak <dennis.colak@pinecrest.edu>
2025-05-27 21:28:34 -0400

    working 3 specimen again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ThreeSpec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ThreeSpec.java
@@ -3,12 +3,11 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.roadrunner.AngularVelConstraint;
-import com.acmerobotics.roadrunner.CompositeVelConstraint;
 import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -19,13 +18,18 @@ import org.firstinspires.ftc.teamcode.Robot;
 import java.util.Arrays;
 
 
-@Autonomous(name = "FourSpec", group = "Auto")
-public class FourSpecRoadrunnerAuto extends LinearOpMode {
+@Autonomous(name = "3 Specimen", group = "Autonomous")
+public class ThreeSpec extends LinearOpMode {
     Robot robot = new Robot(this);
 
     MinVelConstraint forcedSlowVelocity = new MinVelConstraint(Arrays.asList(
             new TranslationalVelConstraint(40.0),
             new AngularVelConstraint(Math.PI / 4)
+    ));
+
+    MinVelConstraint normalVelocity = new MinVelConstraint(Arrays.asList(
+            new TranslationalVelConstraint(80.0),
+            new AngularVelConstraint(Math.PI)
     ));
 
     @Override
@@ -59,55 +63,53 @@ public class FourSpecRoadrunnerAuto extends LinearOpMode {
 
                         // push leftmost spec to observation zone
                         .setReversed(false)
-                        .splineToConstantHeading(new Vector2d(37, -40), Math.toRadians(90))
-                        .strafeTo(new Vector2d(37, -10))
-                        .splineToSplineHeading(new Pose2d(42.75, -2, Math.toRadians(0)), Math.toRadians(0), new AngularVelConstraint(Math.PI / 1.5))
-                        .splineToSplineHeading(new Pose2d(48, -10, Math.toRadians(90)), Math.toRadians(270), new AngularVelConstraint(Math.PI / 1.5))
-                        .splineToConstantHeading(new Vector2d(46, -57), Math.toRadians(270))
+                        .splineToConstantHeading(new Vector2d(35, -40), Math.toRadians(90))
+                        .strafeTo(new Vector2d(35, -12))
+                        // TODO - robot jiggles a bit here. maybe force slower velocity or make it splineToLinearHeading without losing angular velocity continuity
+                        .splineToSplineHeading(new Pose2d(41.5, -5, Math.toRadians(0)), Math.toRadians(0), new AngularVelConstraint(Math.PI / 1.2))
+                        .splineToSplineHeading(new Pose2d(48, -12, Math.toRadians(90)), Math.toRadians(270), new AngularVelConstraint(Math.PI / 1.2))
+                        .splineToConstantHeading(new Vector2d(53, -57), Math.toRadians(270), normalVelocity)
 
-                        // push middle spec to observation zone
-                        .strafeTo(new Vector2d(46, -15))
-                        .splineToConstantHeading(new Vector2d(49.5, -8), Math.toRadians(0), new TranslationalVelConstraint(40.0))
-                        .splineToConstantHeading(new Vector2d(53, -15), Math.toRadians(270), new TranslationalVelConstraint(40.0))
-                        .strafeTo(new Vector2d(53, -54))
+//                        // push middle spec to observation zone
+//                        .strafeTo(new Vector2d(46, -15))
+//                        .splineToConstantHeading(new Vector2d(50.5, -8), Math.toRadians(0), new TranslationalVelConstraint(50.0))
+//                        .splineToConstantHeading(new Vector2d(55, -15), Math.toRadians(270), new TranslationalVelConstraint(50.0))
+//                        .strafeTo(new Vector2d(58, -54), normalVelocity)
 
                         // pickup spec #2
-                        .setTangent(Math.toRadians(125))
-                        .splineToConstantHeading(new Vector2d(38, -72 + 3), Math.toRadians(280), new TranslationalVelConstraint(40.0))
+                        // TODO - middle spec stays in front of robot and stops wheels from touching wall
+                        .strafeTo(new Vector2d(53, -49))
+                        .splineToConstantHeading(new Vector2d(48, -49), Math.toRadians(135), new TranslationalVelConstraint(50.0))
+                        .splineToConstantHeading(new Vector2d(42, -72 + 3), Math.toRadians(280), new TranslationalVelConstraint(50.0))
                         .stopAndAdd(robot.pickupSpecimen())
                         .afterTime(0.2, robot.linearSlide.raiseLinearSlideToHangSpecimen())
 
                         // hang spec #2
                         .setTangent(Math.toRadians(135))
-                        .splineToSplineHeading(new Pose2d(-5, -45, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(-5, -45, Math.toRadians(-90)), Math.toRadians(90), normalVelocity)
                         .strafeTo(new Vector2d(-5, -34), forcedSlowVelocity)
                         .stopAndAdd(robot.hangSpecimen())
 
                         // pickup spec #3
                         .setReversed(false)
-                        .splineToSplineHeading(new Pose2d(40, -72 + 15, Math.toRadians(90)), Math.toRadians(270))
-                        .strafeTo(new Vector2d(40, -72 + 3), forcedSlowVelocity)
+                        .splineToSplineHeading(new Pose2d(38, -50, Math.toRadians(90)), Math.toRadians(0), normalVelocity)
+                        .splineToConstantHeading(new Vector2d(42, -72 + 15), Math.toRadians(-90))
+                        // TODO - not accurate enough
+                        .strafeTo(new Vector2d(42, -72 + 3))
                         .stopAndAdd(robot.pickupSpecimen())
                         .afterTime(0.2, robot.linearSlide.raiseLinearSlideToHangSpecimen())
 
                         // hang spec #3
                         .setTangent(Math.toRadians(135))
+                        // TODO - -2.5 is dangerously close to other specimens. maybe -7.5 or make robot slide a bit before placing specimen
                         .splineToSplineHeading(new Pose2d(-2.5, -45, Math.toRadians(-90)), Math.toRadians(90))
                         .strafeTo(new Vector2d(-2.5, -34), forcedSlowVelocity)
                         .stopAndAdd(robot.hangSpecimen())
 
-//                        // pickup spec #4
-//                        .splineToSplineHeading(new Pose2d(40, -72 + 9, Math.toRadians(90)), Math.toRadians(270))
-//                        .stopAndAdd(robot.pickupSpecimen())
-//                        .afterTime(0.2, robot.linearSlide.raiseLinearSlideToHangSpecimen())
-//
-//                        // hang spec #4
-//                        .setReversed(false)
-//                        .splineToSplineHeading(new Pose2d(-1.5, -35, Math.toRadians(270)), Math.toRadians(90))
-//                        .stopAndAdd(robot.hangSpecimen())
-//
-//                        // park
-//                        .strafeTo(new Vector2d(60, -55))
+                        // park TODO - not tested
+                        .splineToConstantHeading(new Vector2d(65, -65), Math.toRadians(0), new TranslationalVelConstraint(100), new ProfileAccelConstraint(-100, 100))
+
+
                         .build());
     }
 }
